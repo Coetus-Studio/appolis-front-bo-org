01a72fde3084b60c23a85bb816972642ab1bae7e
pabloandrefh <pablovalego@gmail.com>
2025-01-17 14:20:08 -0300

    feat/ add google-maps components

--- src/app/shared/components/map-org/locations.component.ts
+++ src/app/shared/components/map-org/locations.component.ts
@@ -1,13 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { Location } from '../../interfaces/locations.interface';
 import { CommonModule } from '@angular/common';
+
+import { Location } from '../../interfaces/locations.interface';
 import { LocationsService } from '../../services/locations.service';
 
 @Component({
   selector: 'app-locations',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ CommonModule, GoogleMap ],
   templateUrl: './locations.component.html',
   styleUrl: './locations.component.css'
 })
@@ -16,12 +18,17 @@ export default class LocationsComponent {
   // forma nativa de agregar atributos privados
   readonly #locationsService = inject(LocationsService);
 
+  center = signal<google.maps.LatLngLiteral>({ lat: 4.6484784, lng: -74.272619 });
+  zoom = signal(4);
+
+
+
   // constructor (
   //   private locationsService: LocationsService
   // ) {}
 
-  // observable del service
-  locations$ = this.#locationsService.getAllLocations({ lat: 0, lng: 0 });
+  // observable del service, le pasamos el signal center
+  locations$ = this.#locationsService.getAllLocations(this.center());
   $locations = toSignal(this.locations$, {
     initialValue: [],
   });
